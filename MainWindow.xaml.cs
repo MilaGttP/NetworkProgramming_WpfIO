1202ed803eabae2bf10bd3346a9e0d2a9fe44a16
Mila <milaivanova2609@gmail.com>
2023-01-27 19:15:56 +0200

    Completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +22,52 @@ namespace NetworkProgramming_WpfIO
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start1Btn_Click(object sender, RoutedEventArgs e)
+        {
+            const string ip_user = "127.0.0.1";
+            const int port = 8000;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(ip_user);
+            IPEndPoint ep = new IPEndPoint(ip, port);
+            s.Bind(ep);
+            s.Listen(15);
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    TB.Text = $"[{ns.RemoteEndPoint.ToString()}]: Hello server!";
+                    ns.Send(System.Text.Encoding.Unicode.GetBytes("Hello client!"));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void Start2Btn_Click(object sender, RoutedEventArgs e)
+        {
+            const string ip_user = "127.0.0.1";
+            const int port = 8000;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(ip_user);
+            IPEndPoint ep = new IPEndPoint(ip, port);
+            s.Bind(ep);
+            s.Listen(15);
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    TB.Text = $"[{ns.RemoteEndPoint.ToString()}]: Connected";
+                    ns.Send(System.Text.Encoding.Unicode.GetBytes(DateTime.Now.ToString()));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex) { MessageBox.Show(ex.Message); }
         }
     }
 }
