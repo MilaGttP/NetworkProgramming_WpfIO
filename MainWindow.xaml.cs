2f9e1a9d9281e4c9173b168866473b1fada65728
Mila <milaivanova2609@gmail.com>
2023-01-27 19:11:15 +0200

    Completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +22,38 @@ namespace NetworkProgramming_WpfIO
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            const string ipUser = "127.0.0.1";
+            const int port = 8000;
+            IPAddress ip = IPAddress.Parse(ipUser);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    String strSend = "GET\r\n\r\n";
+                    socket.Send(Encoding.Unicode.GetBytes(strSend));
+                    byte[] buffer = new byte[1024];
+                    int l;
+                    do
+                    {
+                        l = socket.Receive(buffer);
+                        TB.Text = Encoding.Unicode.GetString(buffer, 0, l);
+                    } while (l > 0);
+                }
+                else Console.WriteLine("Error");
+            }
+            catch (SocketException ex) { Console.WriteLine(ex.Message); }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
         }
     }
 }
