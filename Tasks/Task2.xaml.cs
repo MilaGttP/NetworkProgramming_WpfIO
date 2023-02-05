fb905f663fcebf8ec67858ffdd683358d2495f1a
Mila <milaivanova2609@gmail.com>
2023-02-05 12:52:15 +0200

    Completed

---
+++ Tasks/Task2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+
+namespace NetworkProgramming_WpfIO
+{
+    public partial class Task2 : Window
+    {
+        HttpClient client;
+        public Task2()
+        {
+            InitializeComponent();
+            client= new HttpClient();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            if (responseBody != null) TB.Text = responseBody;
+        }
+    }
+}
