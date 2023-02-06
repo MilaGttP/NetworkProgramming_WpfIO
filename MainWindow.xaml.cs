3a9979799f04cbe19a03284f93f5e87c956c069f
Mila <milaivanova2609@gmail.com>
2023-02-06 19:59:40 +0200

    Completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Mail;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NetworkProgramming_WpfIO
 {
@@ -20,6 +9,30 @@ namespace NetworkProgramming_WpfIO
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(txtFrom.Text);
+                message.To.Add(txtTo.Text);
+                message.Body = txtMessage.Text;
+                message.IsBodyHtml = true;
+                message.Priority = MailPriority.Normal;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+
+                MessageBox.Show("Mail Sent Successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
