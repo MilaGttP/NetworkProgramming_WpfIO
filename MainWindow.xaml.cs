1762d6a0f5c1f6be0fe0bb15631bc6ff384a6f3f
Mila <milaivanova2609@gmail.com>
2023-02-07 19:47:44 +0200

    First ver

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Mail;
 using System.Windows;
 
@@ -15,18 +16,24 @@ namespace NetworkProgramming_WpfIO
         {
             try
             {
-                MailMessage message = new MailMessage();
-                message.From = new MailAddress(txtFrom.Text);
-                message.To.Add(txtTo.Text);
-                message.Body = txtMessage.Text;
-                message.IsBodyHtml = true;
-                message.Priority = MailPriority.Normal;
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.EnableSsl = true;
-                smtp.Send(message);
-
+                MailAddress fromAddress = new MailAddress(txtFrom.Text);
+                MailAddress toAddress = new MailAddress(txtTo.Text);
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, pass.Name)
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Body = txtMessage.Text
+                })
+                {
+                    smtp.Send(message);
+                }
                 MessageBox.Show("Mail Sent Successfully!");
             }
             catch (Exception ex)
